a0df25df17325765e285a3c49b29b70a85fc17f7
MaDHouSe <madhouse1979@gmail.com>
2025-04-22 16:15:08 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,7 +12,8 @@ version '1.0.0'
 shared_scripts {
     '@qb-core/shared/locale.lua',
     'locales/en.lua', -- change en to your language
-    'config.lua',
+    'shared/config.lua',
+    'shared/vehicles.lua',
 }
 
 client_scripts {
